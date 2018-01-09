852c519cdd897d49e3ec617b82485dc526cb2296
Kevin-Lanzing <klanzing@gmail.com>
2018-01-09 00:23:59 -0500

    Knockout.js filter now updates in real time

--- js/DuluthBBQ.js
+++ js/DuluthBBQ.js
@@ -3,7 +3,7 @@
 // Create a Google Map variable
 var map;
 // Create an empty array of markers
-var markers = [];
+//var markers = [];
 
 // Store the Foursqare client ID and secret for future reference
 var client_id = 'SFLIZ3Z0VXO4TXM5C3UUUUETPD4ZZIO5QE1O2LKLHTXLBDUE';
@@ -18,14 +18,25 @@ var initialURL = 'https://api.foursquare.com/v2/venues/search?' +
 function viewModel() {
   var self = this;
 
-  this.searchEntry = ko.observable('');
-	populateRestaurants(initialURL);
+  this.searchEntry = ko.observable("");
+  this.markers = [];
+  // Initialize the map within the div
+  this.initMap = function() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 33.958681, lng: -84.1363947},
+      zoom: 15
+    });
+    // Create an infowindow to display when a marker is clicked
+  	this.largeInfowindow = new google.maps.InfoWindow();
+    populateRestaurants(initialURL, this.markers);
+  }
 
   this.searchFilter = ko.computed(function() {
+    console.log('searchFilter has been called: ' + this.searchEntry());
     var result = [];
-    for (var i = 0; i < markers.length; i++) {
-      var marker = markers[i];
-      if (marker.title.toLowerCase().includes(searchEntry().toLowerCase())) {
+    for (var i = 0; i < this.markers.length; i++) {
+      var marker = this.markers[i];
+      if (marker.title.toLowerCase().includes(this.searchEntry().toLowerCase())) {
         result.push(marker);
         this.markers[i].setVisible(true);
       } else {
@@ -36,43 +47,46 @@ function viewModel() {
     return result;
   }, this);
 
-  // Initialize the map within the div
-  this.initMap = function() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 33.958681, lng: -84.1363947},
-      zoom: 15
+  function populateRestaurants(query, markers) {
+  // Using Foursquare, place 10 markers matching query.
+    $.getJSON(query, function(data) {
+      var results = data.response.venues;
+      for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        var position = result.location;
+        var title = result.name;
+        var foursquareID = result.id;
+        this.marker = new google.maps.Marker({
+          position: position,
+          title: title,
+          animation: google.maps.Animation.DROP,
+          id: i,
+          foursquareID: foursquareID
+        });
+        this.marker.setMap(map);
+        markers.push(this.marker);
+        this.marker.addListener('click', function() {
+          view.prepareInfoWindow(this, largeInfowindow);
+        });
+      }
     });
-    console.log("initMap has been called");
-    // Create an infowindow to display when a marker is clicked
-  	this.largeInfowindow = new google.maps.InfoWindow();
+  }
+  function showListings(restaurants) {
+    var bounds = new google.maps.LatLngBounds();
+    // Extend the boundaries of the map for each marker and display the marker
+    for (var i = 0; i < restaurants.length; i++) {
+      if (restaurants[i].title.toLowerCase().includes(searchEntry().toLowerCase())) {
+        restaurants[i].setMap(map);
+        restaurants[i].setVisible(true);
+        bounds.extend(restaurants[i].position);
+        console.log(restaurants[i].title.toLowerCase());
+      }
+    }
+    map.fitBounds(bounds);
   }
   this.initMap();
 }
 
-function populateRestaurants(query) {
-  // Using Foursquare, place 10 markers matching query.
-  $.getJSON(query, function(data) {
-    var results = data.response.venues;
-    for (var i = 0; i < results.length; i++) {
-      var result = results[i];
-      var position = result.location;
-      var title = result.name;
-      var foursquareID = result.id;
-      var marker = new google.maps.Marker({
-        position: position,
-        title: title,
-        animation: google.maps.Animation.DROP,
-        id: i,
-        foursquareID: foursquareID
-      });
-      markers.push(marker);
-      marker.addListener('click', function() {
-        view.prepareInfoWindow(this, largeInfowindow);
-      });
-    }
-          //view.populateSidebar(markers);
-  });
-}
 
 // Let's get started by initializing our viewmodel using Knockout
 function getStarted() {
